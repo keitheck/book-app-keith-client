29ec9241e16e28600de8b29749ec9033fc10028e
keitheck <eckertkeith@gmail.com>
2018-01-28 15:06:48 -0800

    first ajax route test

---
+++ scripts/views/newBook.js
@@ -0,0 +1,26 @@
+'use strict';
+
+pageLoad();
+
+$('#newBookForm').on('submit', function(e) {
+  e.preventDefault();
+  //captures from data
+  let data = {
+    author: e.target.author.value,
+    title: e.target.title.value,
+    isbn: e.target.isbn.value,
+    image_url: e.target.image.url.value,
+    description: e.target.description.value,
+  }
+
+  //AJAX CALL HERE TO POST TO DB
+
+});
+
+function pageLoad() {
+  $.get('/db/books')
+  .then(function(data) {
+    console.log('pageLoad data from db/books', data);
+  });
+
+}
