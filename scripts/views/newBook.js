03f0e7bdcfef9fdc64dcf907f9b5b11d796a7521
keitheck <eckertkeith@gmail.com>
2018-01-28 19:12:03 -0800

    get routes added

--- scripts/views/newBook.js
+++ scripts/views/newBook.js
@@ -17,13 +17,16 @@ $('#newBookForm').on('submit', function(e) {
     description: e.target.description.value,
   }
 
-  $.post(`${__API_URL__}/db/books`, data)
+  $.post(`${__API_URL__}/books`, data)
     .then(function() {
       pageLoad();
     })
     .catch(function(err) {
       console.error("newbookform error", err);
       pageLoad();
+    });
+    $('#newBookForm').each(function() {
+      this.reset();
     });
     
   //AJAX CALL HERE TO POST TO DB
@@ -33,7 +36,7 @@ $('#newBookForm').on('submit', function(e) {
 
 // this AJAX request asks server for DB to respond with all book data
 function pageLoad() {
-  $.get(`${__API_URL__}/db/books`)
+  $.get(`${__API_URL__}/books`)
   .then(function(data) {
     console.log('pageLoad data from db/books', data);
     // $('#results').empty();
